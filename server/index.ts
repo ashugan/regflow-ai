49fb40887110f8883dce965b636de41577e77387
Andrew Shugan <shugan@mac.mynetworksettings.com>
2026-05-14 15:11:31 -0400

    Use Render assigned port

--- server/index.ts
+++ server/index.ts
@@ -344,9 +344,8 @@ app.delete("/documents/:id", (req, res) => {
     );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-// Starts Express API server.
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
